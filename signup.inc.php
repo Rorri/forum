f177a37b2e19a89e8d43a851c06af9960ce1f4b6
Rorri <37700299+Rorri@users.noreply.github.com>
2018-05-12 16:07:40 +0200

    Update signup.inc.php

--- signup.inc.php
+++ signup.inc.php
@@ -1,28 +1,26 @@
 <?php
-
-if (isset($_POST['submit'])) {
-
+if (isset($_POST['submit'])) 
+{
 	include_once 'dbh.inc.php';
-
+	
 	$first = mysqli_real_escape_string($conn, $_POST['first']);
 	$last = mysqli_real_escape_string($conn, $_POST['last']);
 	$email = mysqli_real_escape_string($conn, $_POST['email']);
 	$uid = mysqli_real_escape_string($conn, $_POST['uid']);
 	$pwd = mysqli_real_escape_string($conn, $_POST['pwd']);
-
-	//Error hanterare//
-	//Kolla för tomma fält//
+	//Error checkers//
+	//Checking for empty fields//
 	if (empty($first) || empty($last) || empty($email) || empty($uid) || empty($pwd)) {
 		header("Location: ../signup.php?signup=empty");
 		exit();
 	} else {
-		//check if input characters are valid
-		if (!preg_match("/^[a-zA-Z]*$/", $first) || (!preg_match("/^[a-zA-Z]*$/", $last)) 
+		//Checking if input characters are valid
+		if (!preg_match("/^[a-zA-Z]*$/", $first) || (!preg_match("/^[a-zA-Z]*$/", $last)))
 		{
 		header("Location: ../signup.php?signup=invalid");
 		exit();
 	} else {
-		//check if email is valid
+		//Checking if email is valid
 		if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
 			header("Location: ../signup.php?signup=email");
 			exit();
@@ -30,14 +28,13 @@ if (isset($_POST['submit'])) {
 				$sql = "SELECT * FROM users WHERE user_uid='$uid'";
 				$result = mysqli_query($conn, $sql);
 				$resultCheck = mysqli_num_rows($result);
-
 				if ($resultCheck > 0) {
 					header("Location: ../signup.php?signup=invalid");
 					exit();
 				} else {
 					//Hashin the passord//
 					$hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
-					//Insert user into the database
+					//Insert the new user into the database
 					$sql = "INSERT INTO users (user_first, user_last, user_email, user_uid, user_pwd) VALUES ('$first', '$last', '$email', '$uid', '$hashedPwd');";
 					mysqli_query($conn, $sql); 
 					header("Location: ../signup.php?signup=success");
@@ -46,8 +43,7 @@ if (isset($_POST['submit'])) {
 			}
 		}
 	}
-
 } else {
-	header("Location: ../singup.php")
+	header("Location: ../singup.php");
 	exit();
 }
